268a27fa3ab4ca319cc96b7c2b5c51e1aace9d6f
Scoz9 <83401130+Scoz9@users.noreply.github.com>
2022-05-27 16:57:47 +0200

    Animation idle-run-jump-added
    Need to fixup jump glitch

--- GitGame/Assets/Script/KnightController.cs
+++ GitGame/Assets/Script/KnightController.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/* We have to fix the bug that consist in: when the player jumps and at the end falls in the ground, he goes a bit more down respect the ground */
+// Inserire la variabile per l'animator grounded 
+//if grounded = false saltando diventa true e passiamo allo stato idle 
 public class KnightController : MonoBehaviour
 {
     private Rigidbody2D rb; 
@@ -18,14 +21,17 @@ public class KnightController : MonoBehaviour
     private bool isJumping;
     private float jumpTimeCounter;
     public float jumpTime;
+
+    private Animator anim;
+
     
-    // Start is called before the first frame update
     void Start()
     {
+        //Grab references for rigidbody and animator from object
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
         moveInput = Input.GetAxisRaw("Horizontal");
@@ -33,20 +39,34 @@ public class KnightController : MonoBehaviour
     }
 
     void Update(){
-        isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, whatIsGround);
+        
+        KnightMovementRL(moveInput);
+        KinghtJump();
+        
+        //Set animator parameters
+        anim.SetBool("run", moveInput != 0);
+        anim.SetBool("grounded", !isJumping);
+    }
 
+    public void KnightMovementRL(float moveInput){
         if(moveInput > 0){
             transform.eulerAngles = new Vector3(0,0,0);
         } else if(moveInput < 0){
             transform.eulerAngles = new Vector3(0,180,0); 
         }
+    }
 
-        if(isGrounded == true && Input.GetKeyDown(KeyCode.Space)){
+    public void KinghtJump(){
+        isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, whatIsGround);
+
+        //Se il playr è sul pavimento e viene cliccato lo space -> isJumping(sta saltando) ... 
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded == true ){
             isJumping = true;
             jumpTimeCounter = jumpTime;
             rb.velocity = Vector2.up * jumpForce;
         }
 
+        //Se il player sta saltando ed lo spazio è premuto, coltinuerà a saltare per il tempo assegnato 
         if(Input.GetKey(KeyCode.Space) && isJumping == true){
             if(jumpTimeCounter > 0){
                 rb.velocity = Vector2.up * jumpForce;
@@ -61,4 +81,5 @@ public class KnightController : MonoBehaviour
             isJumping = false;
         }
     }
+
 }
