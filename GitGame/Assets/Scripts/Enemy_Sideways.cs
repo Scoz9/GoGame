c819c31a7bf4a6745ff2c9320005cb3413016387
Scoz9 <83401130+Scoz9@users.noreply.github.com>
2022-06-08 15:13:52 +0200

    Sistemazione generale
    Sistemazione cartelle, vari layer degli items e aggiunte trap con movimento.

---
+++ GitGame/Assets/Scripts/Enemy_Sideways.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Sideways : MonoBehaviour
+{
+
+    //[SerializeField] private float damage;
+    [SerializeField] private float speed;
+    [SerializeField] private float range;
+
+    float startingX;
+    int dir = 1;
+
+    void Start()
+    {
+        startingX = transform.position.x;
+    }
+
+    void FixedUpdate()
+    {
+        transform.Translate(Vector2.right * speed * Time.fixedDeltaTime * dir);
+        if(transform.position.x < startingX || transform.position.x > startingX + range)
+            dir *= -1;
+    }
+
+    /*private void OnTriggerEnter2D(Collider2D collision){
+        if(collision.tag == "Player"){
+            collision.GetComponent<Health>().TakeDamage(damage);
+        }
+    }*/
+}
