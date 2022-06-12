9edbba28a0698cb050edc33d4417d7adbaad05ab
Scoz9 <83401130+Scoz9@users.noreply.github.com>
2022-06-12 10:01:46 +0200

    Coins animation + script + canvas aggiornata + PlayerPrefs
    Aggiunti:
    -- coin animati + script
    -- aggiornata la canvas che mostra a schermo i coin raccolti
    -- settate le PlayerPrefs in maniera tale da salvare i coin raccolti

--- GitGame/Assets/Scripts/PlayerManager.cs
+++ GitGame/Assets/Scripts/PlayerManager.cs
@@ -2,13 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class PlayerManager : MonoBehaviour
 {
     public static bool isGameOver;
     public GameObject gameOverScreen;
+    public static int numberOfCoins;
+    public TextMeshProUGUI coinsText;
     public void Awake()
     {
+        numberOfCoins = PlayerPrefs.GetInt("NumberOfCoins", 0);
         isGameOver = false;
 
     }
@@ -16,6 +20,7 @@ public class PlayerManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        coinsText.text = numberOfCoins.ToString();
         if(isGameOver)
         {
             gameOverScreen.SetActive(true);
