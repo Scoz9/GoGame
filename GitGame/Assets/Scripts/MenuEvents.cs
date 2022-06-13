60656ab794f690a30cacdc4b79484782772e1404
Scoz9 <83401130+Scoz9@users.noreply.github.com>
2022-06-13 13:01:49 +0200

    Aggiunto voce menù setting "opzione volume"
    Aggiunto nella voce di menu settings l'opzione volume.

--- GitGame/Assets/Scripts/MenuEvents.cs
+++ GitGame/Assets/Scripts/MenuEvents.cs
@@ -2,9 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.Audio;
 
 public class MenuEvents : MonoBehaviour
 {
+    public Slider volumeSlider;
+    public AudioMixer mixer;
+    private float value;
+
+    public void Start()
+    {
+        mixer.GetFloat("volume",out value); //value deve essere passata con la keyword out
+        volumeSlider.value = value;
+    }
+    
+    public void SetVolume()
+    {
+        mixer.SetFloat("volume", volumeSlider.value);
+    }
     public void LoadLevel(int index)
     {
         SceneManager.LoadScene(index);
