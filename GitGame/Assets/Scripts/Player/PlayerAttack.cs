d427d48a7c9209fdf34c07389f077e34c5e9e22d
Scoz9 <83401130+Scoz9@users.noreply.github.com>
2022-06-29 00:10:06 +0200

    Attaco verso goblin

--- GitGame/Assets/Scripts/Player/PlayerAttack.cs
+++ GitGame/Assets/Scripts/Player/PlayerAttack.cs
@@ -6,9 +6,15 @@ using UnityEngine;
 public class PlayerAttack : MonoBehaviour
 {
     [SerializeField] private float attackCooldown;
+
     private Animator anim;
     private KnightController playerMovement;
     private float cooldownTimer = Mathf.Infinity;
+
+    public Transform attackPos;
+    public LayerMask whatIsEnemies;
+    public float attackRange;
+    public int damage;
     
     private void Awake()
     {
@@ -19,14 +25,22 @@ public class PlayerAttack : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        if(Input.GetMouseButton(0) && cooldownTimer > attackCooldown && playerMovement.canAttack())
+        if(Input.GetKeyDown(KeyCode.C) && cooldownTimer > attackCooldown && playerMovement.canAttack()){
+            Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackRange, whatIsEnemies);
+            for(int i = 0; i < enemiesToDamage.Length; i++) enemiesToDamage[i].GetComponent<Goblin>().TakeDamage(damage);
             Attack();
-
+        }
         cooldownTimer += Time.deltaTime;
     }
 
     private void Attack(){
         anim.SetTrigger("attack");
         cooldownTimer = 0;
+    }
+
+    //Per disegnare il cerchietto rosso relativo all'arma 
+    void OnDrawGizmosSelected(){
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attackPos.position, attackRange);
     }
 }
